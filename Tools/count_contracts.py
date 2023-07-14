27908f719e56e72309b8db1f489479adb903f516
ShivankKhullar <shivankkhullarapps@gmail.com>
2023-07-15 03:45:48 +0530

    Added Creating the CSV file

--- Tools/count_contracts.py
+++ Tools/count_contracts.py
@@ -8,6 +8,8 @@ from prettytable import PrettyTable
 import re
 from benchmark import function_benchmark,print_benchmark_results
 from termcolor import colored
+import datetime
+import csv
 
 directory_path = "../Contracts"
 # directory_path = 'C:/Github/DAppSCAN/DAppSCAN-source/contracts/Ackee_Blockchain-GoodGhosting'
@@ -17,7 +19,7 @@ directory_path = "../Contracts"
 class NodeProcessor:
 
     @staticmethod
-    def calculate_nesting_depth(node, state):
+    def process_node_for_nesting_depth(node, state):
         if node is None:
             state['current_depth'] += 0
         elif node.type in {NodeType.IF, NodeType.STARTLOOP}:
@@ -29,7 +31,7 @@ class NodeProcessor:
         return state
 
     @staticmethod
-    def calculate_function_call(node, state):
+    def process_node_for_function_calls(node, state):
         if isinstance(node.expression, (CallExpression, LibraryCall)):
             state['function_calls'] += 1
         return state
@@ -78,13 +80,13 @@ class ContractAnalyzer:
     def calculate_nesting_depth(self, function):
         self.state['current_depth'] = 0
         self.state['max_depth'] = 0
-        traverser = CFGTraverser(NodeProcessor.calculate_nesting_depth)
+        traverser = CFGTraverser(NodeProcessor.process_node_for_nesting_depth)
         self.state = traverser.tree_traversal(function.entry_point, self.state)
 
     @function_benchmark
     def calculate_function_call(self, function):
         self.state['function_calls'] = 0
-        traverser = CFGTraverser(NodeProcessor.calculate_function_call)
+        traverser = CFGTraverser(NodeProcessor.process_node_for_function_calls)
         self.state = traverser.node_list_traversal(function.nodes, self.state)
 
     def use_solc(self, version):
@@ -105,6 +107,7 @@ class ContractAnalyzer:
                         version = version_match.group()
                         self.use_solc(version)
                     break
+        
         slither = Slither(file_path)
         contracts = slither.contracts
         for contract in contracts:
@@ -133,25 +136,46 @@ class DirectoryProcessor:
                     results[file_name] = contract_results
         return results
 
+class Results:
 
-class ResultPrinter:
-
-    def print_results(self, results):
+    def generate_table(self, results):
         table = PrettyTable()
-        table.field_names = ["File Name", "Number of Contracts", "Contract Name", "Inheritance Depth", "Number of Functions", "Function Name", "Number of Parameters", "Nesting Depth", "Function Calls"]
+        table.field_names = ["File Name", "Number of Contracts", "Contract Name", "Inheritance Depth", 
+                             "Number of Functions", "Function Name", "Number of Parameters", 
+                             "Nesting Depth", "Function Calls"]
         for file_name, contract_results in results.items():
             for contract_name, data in contract_results.items():
                 function_results, inheritance_depth = data
                 for function_name, function_data in function_results.items():
                     parameter_count, nesting_depth, function_calls = function_data
-                    table.add_row([file_name, len(contract_results), contract_name, inheritance_depth, len(function_results), function_name, parameter_count, nesting_depth, function_calls])
+                    table.add_row([file_name, len(contract_results), contract_name, inheritance_depth, 
+                                   len(function_results), function_name, parameter_count, 
+                                   nesting_depth, function_calls])
+        return table
+
+    def print_table(self, table):
         print(colored(table, 'light_cyan'))
 
+    def save_table_to_csv(self, table, directory_path):
+        timestamp = datetime.datetime.now().strftime('%d-%m-%Y_%H-%M-%S')
+        filename = f'../csv_results/results_{timestamp}.csv'
+        header = f'These metrics have been extracted from all contracts with the parent directory as {directory_path} on {timestamp}\n'
+        
+        with open(filename, 'w', newline='') as f:  # add newline='' here
+            f.write(header)
+            writer = csv.writer(f)
+            writer.writerow(table.field_names)  # write the header row
+            for row in table._rows:  # iterate through the data rows
+                writer.writerow(row)  # write each row    
 
 directory_processor = DirectoryProcessor()
-results = directory_processor.process_directory(directory_path)
+results_dictionary = directory_processor.process_directory(directory_path)
 
-result_printer = ResultPrinter()
-result_printer.print_results(results)
+results = Results()
+table = results.generate_table(results_dictionary)
+
+results.print_table(table)
+
+# results.save_table_to_csv(table, directory_path)
 
 print_benchmark_results()
